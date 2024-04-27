3ce2baa42da8b1eda823059523f1f450df69777c
nikhilmittalx <nikhilmittal421@gmail.com>
2024-04-28 00:17:16 +0530

    ..

---
+++ src/assets/images/index.js
@@ -0,0 +1,15 @@
+
+import menu from "./menu.svg";
+import close from "./close.svg";
+import wind from "./wind.svg";
+import humid from "./humidity.svg";
+import temp from "./temperature.svg";
+export {
+	
+	wind,
+	humid,
+	temp,
+	// sun,
+	menu,
+	close,
+};
---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import TopButtons from "./TopButtons";
+import Inputs from "./Inputs";
+import TimeAndLocation from "./TimeAndLocation";
+import TemperatureAndDetails from "./TemperatureAndDetails";
+import SunriseAndSunset from "./SunriseAndSunset";
+import Forecast from "./Forecast";
+export {
+	Forecast,
+	SunriseAndSunset,
+	TemperatureAndDetails,
+	TimeAndLocation,
+	TopButtons,
+	Inputs,
+};
